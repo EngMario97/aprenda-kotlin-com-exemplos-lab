906cd8d54d8f2b641aa3d9e1200793a56171c103
Mario Rocha <93800556+EngMario97@users.noreply.github.com>
2023-05-11 11:47:41 -0300

    Solution desafio.kt
    Solução do desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,77 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+class Formacao(val nome: String, var nivel: Nivel, var conteudos: MutableList<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        if (!inscritos.contains(usuario)) {
+            inscritos.add(usuario)
+            println("Usuário ${usuario.nome} matriculado na formação $nome.")
+        } else {
+            println("Usuário ${usuario.nome} já está matriculado na formação $nome.")
+        }
+    }
+
+    fun cancelarMatricula(usuario: Usuario) {
+        if (inscritos.contains(usuario)) {
+            inscritos.remove(usuario)
+            println("Usuário ${usuario.nome} cancelou sua matrícula na formação $nome.")
+        } else {
+            println("Usuário ${usuario.nome} não está matriculado na formação $nome.")
+        }
+    }
+
+    fun adicionarConteudo(conteudo: ConteudoEducacional) {
+        conteudos.add(conteudo)
+        println("Conteúdo ${conteudo.nome} adicionado à formação $nome.")
+    }
+
+    fun removerConteudo(conteudo: ConteudoEducacional) {
+        if (conteudos.contains(conteudo)) {
+            conteudos.remove(conteudo)
+            println("Conteúdo ${conteudo.nome} removido da formação $nome.")
+        } else {
+            println("Conteúdo ${conteudo.nome} não está presente na formação $nome.")
+        }
+    }
+
+    fun exibirConteudos() {
+        println("Conteúdos da formação $nome:")
+        conteudos.forEach { conteudo ->
+            println(" - ${conteudo.nome} (${conteudo.duracao} minutos)")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("Mario")
+    val usuario2 = Usuario("Maria")
+
+    val conteudo1 = ConteudoEducacional("Introdução a programação", 30)
+    val conteudo2 = ConteudoEducacional("Estruturas de dados", 70)
+    val conteudo3 = ConteudoEducacional("Algoritmos de busca", 45)
+
+    val formacao1 = Formacao("Desenvolvimento Web", Nivel.INTERMEDIARIO, mutableListOf(conteudo1))
+    formacao1.adicionarConteudo(conteudo2)
+    formacao1.adicionarConteudo(conteudo3)
+    formacao1.exibirConteudos()
+
+    formacao1.matricular(usuario1)
+    formacao1.matricular(usuario2)
+    formacao1.matricular(usuario2)
+
+    formacao1.cancelarMatricula(usuario1)
+    formacao1.cancelarMatricula(usuario1)
+
+    val formacao2 = Formacao("Inteligência Artificial", Nivel.DIFICIL, mutableListOf(conteudo2))
+    formacao2.adicionarConteudo(conteudo3)
+    formacao2.exibirConteudos()
+
+    formacao2.matricular(usuario1)
+    formacao2.matricular(usuario2)
 }
